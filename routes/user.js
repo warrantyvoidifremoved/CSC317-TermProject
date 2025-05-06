d39c215411e552ab36240d4e2366131c309c75c8
warrantyvoidifremoved <rahulssrinath@gmail.com>
2025-05-06 03:55:54 -0700

    Updated file names

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // User profile page
 router.get('/', (req, res) => {
-    res.render('user-profile', {
+    res.render('user', {
         title: 'Profile',
         username: 'Foo Bar',
         currentOrders: [],
