981b763a2359b4f8b2c167137a8b0eab6ba63743
warrantyvoidifremoved <rahulssrinath@gmail.com>
2025-05-21 19:51:19 -0700

    Redo mobile navbar styling

---
+++ public/js/layout.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('mobile-menu-toggle');
+const mobileNavLinks = document.querySelector('.mobile-nav');
+
+menuToggle.addEventListener('click', () => {
+    mobileNavLinks.classList.toggle('open');
+});
